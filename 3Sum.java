2625861501f6365c3d9eaa5e319dbb64a231ffa7
Ankur Yadav <36620764+yankur2110@users.noreply.github.com>
2020-05-02 21:19:15 +0530

    Add files via upload

---
+++ 3Sum.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        
+        for(int i=0;i<nums.length-2;i++){
+            
+            if(i==0 || (i>0 && nums[i]!=nums[i-1])){ //to skip repeated elements in start
+                int low = i+1;
+                int high = nums.length-1;
+
+                int reqdSum = 0-nums[i];
+
+                while(low<high){
+                    if(nums[low]+nums[high]==reqdSum){//if sum is equal
+                        result.add(Arrays.asList(nums[i],nums[low],nums[high]));
+                        while(low<high && nums[low]==nums[low+1]){ /*to skip repeated elements in bucket*/
+                            low++;
+                        }
+                        while(low<high && nums[high]==nums[high-1]){ /*to skip repeated elements in bucket*/
+                            high--;
+                        }
+                        
+                        low++;
+                        high--;
+                    }else if(nums[low]+nums[high]>reqdSum){//if sum is low
+                        high--;
+                    }
+                    else if(nums[low]+nums[high]<reqdSum){//if sum is high
+                        low++;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
