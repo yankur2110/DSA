5526abc67361cf50f5da842bf1ee91685123c5e2
Ankur Yadav <36620764+yankur2110@users.noreply.github.com>
2020-05-19 23:37:55 +0530

    Add files via upload

---
+++ Maximum Subarray.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        //better solution is through recursion,eventhough I didn't see
+        
+        int max_so_far =Integer.MIN_VALUE;
+        int max_ending_here =0;
+        
+        for(int i=0;i<nums.length;i++){
+         
+            max_ending_here = max_ending_here+nums[i];
+            max_so_far=Math.max(max_so_far,max_ending_here);
+            if(max_ending_here<0)
+                max_ending_here = 0;
+            
+        }
+        
+        return max_so_far;
+        
+        
+    }
+}
