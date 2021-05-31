0158e9f39218f41cd0a3addabd7c102460758a69
Ankur Yadav <36620764+yankur2110@users.noreply.github.com>
2021-05-31 22:43:37 +0530

    Add files via upload

---
+++ Reverse Bits.java
@@ -0,0 +1,18 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+       
+      int  result = 0;
+        
+        for(int i=0;i<32;i++){
+            
+            int lsb = n&1;
+            int reverseLsb=lsb<<(31-i);
+            n=n>>1;
+            result = result | reverseLsb;
+            
+        }
+        
+     return result;
+    }
+}
