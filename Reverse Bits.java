09aa4b84248499f9f88a093038c667f11fc3cd5d
Ankur Yadav <36620764+yankur2110@users.noreply.github.com>
2021-05-31 22:54:09 +0530

    Add files via upload

--- Reverse Bits.java
+++ Reverse Bits.java
@@ -1,18 +1,14 @@
 public class Solution {
-    // you need treat n as an unsigned value
-    public int reverseBits(int n) {
-       
-      int  result = 0;
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
         
+        int count =0;
         for(int i=0;i<32;i++){
-            
-            int lsb = n&1;
-            int reverseLsb=lsb<<(31-i);
-            n=n>>1;
-            result = result | reverseLsb;
-            
+            int m = n&1;
+            if(m==1)
+                count++;
+            n>>=1;
         }
-        
-     return result;
+        return count;
     }
 }
