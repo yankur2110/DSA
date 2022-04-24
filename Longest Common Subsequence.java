7817f620034f61cd7838cda8474dc0912197fd91
Ankur Yadav <36620764+yankur2110@users.noreply.github.com>
2022-04-24 19:57:22 +0530

    Add files via upload

---
+++ Longest Common Subsequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length(), n = text2.length();
+
+        int[][] dp = new int[m+1][n+1]; //DP array.
+
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                char ch1 = text1.charAt(i-1), ch2 = text2.charAt(j-1); //current character in both the strings.
+                dp[i][j] = (ch1==ch2)? 1+dp[i-1][j-1] : Math.max(dp[i][j-1],dp[i-1][j]);//if current characters are same then LCS would be one more than the LCS of strings when current character is not included in both strings.
+                //if different, then it would be max of (don't include one char from s2, don't include one char from s1)
+
+            }
+        }
+
+        return dp[m][n];
+
+    }
+}
