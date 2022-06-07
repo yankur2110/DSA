dcf2df04d6136451721ae096ccc13b9e8243b1fe
Ankur Yadav <36620764+yankur2110@users.noreply.github.com>
2022-06-07 14:53:28 +0530

    Add files via upload

---
+++ Lowest Common Ancestor of a Binary Search Tree.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //main thing to notice here is that to find LCA of p and q we have to find the last node till which both of them exist in the same subtree of this node (and this node would be LCA).
+
+        while (true) {
+            if (root.val > p.val && root.val > q.val)
+                root = root.left;
+            else if (root.val < p.val && root.val < q.val)
+                root = root.right;
+            else
+                return root;
+        }
+    }
+}
