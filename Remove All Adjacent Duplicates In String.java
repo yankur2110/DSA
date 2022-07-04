a24257e4211625aef449baa2df5d22a69356410a
Ankur Yadav <36620764+yankur2110@users.noreply.github.com>
2022-07-04 21:45:09 +0530

    Add files via upload

---
+++ Remove All Adjacent Duplicates In String.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> stack  =  new Stack<>();
+
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+
+            if(!stack.isEmpty() && stack.peek()==ch){
+                while(!stack.isEmpty() && stack.peek()==ch){
+                    stack.pop();
+                }
+            }else{
+                stack.push(ch);
+            }
+        }
+
+        StringBuilder strBuilder = new StringBuilder();
+        while(!stack.isEmpty()){
+            strBuilder.append(stack.pop());
+        }
+
+        return strBuilder.reverse().toString();
+    }
+}
