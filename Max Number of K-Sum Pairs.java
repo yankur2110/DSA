19287c728ae74131ca0aed8149a562994c3f205c
Ankur Yadav <36620764+yankur2110@users.noreply.github.com>
2022-08-08 21:24:13 +0530

    Add files via upload

---
+++ Max Number of K-Sum Pairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int start=0, end=nums.length-1;
+        int count=0;
+
+        while(start<end){
+            int sum=nums[start]+nums[end];
+            if(sum==k){
+                start++;end--;
+                count++;
+            }else if(sum<k){
+                start++;
+            }else{
+                end--;
+            }
+        }
+
+        return count;
+    }
+}
