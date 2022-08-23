f53214b63a08a676d4eefbe3a2adbf2cf73a597d
Ankur Yadav <36620764+yankur2110@users.noreply.github.com>
2022-08-23 14:46:28 +0530

    Add files via upload

---
+++ Count Good Nodes in Binary Tree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count=0;
+    public int goodNodes(TreeNode root) {
+        dfs(root,Integer.MIN_VALUE);
+        return count;
+    }
+
+    public void dfs(TreeNode root, int maxTillRoot){
+        if(root==null)
+            return;
+
+        if(root.val>=maxTillRoot){
+            count++;
+        }
+
+        maxTillRoot = Math.max(root.val, maxTillRoot);
+
+        dfs(root.left, maxTillRoot);
+        dfs(root.right, maxTillRoot);
+    }
+}
